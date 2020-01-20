eccfa3dc0cce6f69c02a74019b02d6a6ac1a7a65
Colette Shaw <CShaw@spartaglobal.com>
2020-01-20 14:42:32 +0000

    Add 5th lab

---
+++ labs/lab_05_log_to_event_viewer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace lab_05_log_to_event_viewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventLog.WriteEntry("Application", "Your system is about to expire.", EventLogEntryType.Error, 5000, 1234);
+        }
+    }
+}
