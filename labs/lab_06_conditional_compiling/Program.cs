cbcde939d7176e15fb60dcd0db335af0c4eea997
Colette Shaw <CShaw@spartaglobal.com>
2020-01-20 14:58:39 +0000

    Add lab 6

---
+++ labs/lab_06_conditional_compiling/Program.cs
@@ -0,0 +1,23 @@
+﻿#define THISISTRUE
+using System;
+
+namespace lab_06_conditional_compiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("always printed");
+
+#if DEBUG
+            Console.WriteLine("this is debug code");
+#else
+            Console.WriteLine("this is production code");
+#endif
+#if THISISTRUE
+            Console.WriteLine("this is true");
+#endif
+
+        }
+    }
+}
